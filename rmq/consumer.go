14fb77f0e3a39f9f61258974b2b8c542853c5dbc
liusiming <liusiming@5eplay.com>
2022-09-13 16:06:35 +0800

    config备注

--- rmq/consumer.go
+++ rmq/consumer.go
@@ -8,13 +8,13 @@ import (
 )
 
 type ConsumerConfig struct {
-	NameSrvAddr    []string `json:"name_srv_addr" yaml:"name_srv_url"`
-	GroupName      string   `json:"group_name,omitempty" yaml:"group_name"`
-	ConsumerModel  int      `json:"consumer_model,omitempty" yaml:"consumer_model"`
-	UseCredentials bool     `json:"use-credentials,omitempty" yaml:"use_credentials"`
-	AccessKey      string   `json:"access_key,omitempty" yaml:"access_key"` // AccessKey 阿里云身份验证，在阿里云服务器管理控制台创建
-	SecretKey      string   `json:"secret_key,omitempty" yaml:"secret_key"` // SecretKey 阿里云身份验证，在阿里云服务器管理控制台创建
-	ConsumerOrder  bool     `json:"consumer_order,omitempty" yaml:"consumer_order"`
+	NameSrvAddr    []string `json:"name_srv_addr" yaml:"name_srv_url"`                // name srv地址
+	GroupName      string   `json:"group_name,omitempty" yaml:"group_name"`           // 组名
+	ConsumerModel  int      `json:"consumer_model,omitempty" yaml:"consumer_model"`   // 0-广播模式 1-集群模式
+	UseCredentials bool     `json:"use-credentials,omitempty" yaml:"use_credentials"` // 是否需要身份验证
+	AccessKey      string   `json:"access_key,omitempty" yaml:"access_key"`           // AccessKey 阿里云身份验证，在阿里云服务器管理控制台创建
+	SecretKey      string   `json:"secret_key,omitempty" yaml:"secret_key"`           // SecretKey 阿里云身份验证，在阿里云服务器管理控制台创建
+	ConsumerOrder  bool     `json:"consumer_order,omitempty" yaml:"consumer_order"`   // 是否顺序消费
 }
 
 func NewConsumer(c *ConsumerConfig) (rocketmq.PushConsumer, error) {
