27a75099eec848ab20bf2a27989fb6b7ba88b9e3
liusiming <liusiming@5eplay.com>
2022-09-16 15:38:21 +0800

    rmq example

--- rmqlog/consumer.go
+++ rmqlog/consumer.go
@@ -1,15 +1,5 @@
 package rmqlog
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/apache/rocketmq-client-go/v2"
-	"github.com/apache/rocketmq-client-go/v2/consumer"
-	"github.com/apache/rocketmq-client-go/v2/primitive"
-	"github.com/gowins/dionysus/rmq"
-)
-
 // &rmq.ConsumerConfig{
 // NameSrvAddr:    []string{"http://rmq-cn-zvp2ud6lc0e.cn-hangzhou.rmq.aliyuncs.com:8080"},
 // GroupName:      "hltv_g",
@@ -19,28 +9,29 @@ import (
 // SecretKey:      "3yuDlpGFwL2itNkAx2Djxe+XcK1BDLC1VFqD6b7CV+A=",
 // ConsumerOrder:  false,
 // }
-type Cons struct {
-	clientConsumer rocketmq.PushConsumer
-}
 
-var Cs Cons
-
-func Initial(c *rmq.ConsumerConfig) (*Cons, error) {
-	if c, err := rmq.NewConsumer(c); err == nil {
-		Cs.clientConsumer = c
-		return &Cs, nil
-	} else {
-		return nil, err
-	}
-}
-
-func (c *Cons) SetTopicConsumer(topic string, handler func(ext *primitive.MessageExt)) {
-	cc := c.clientConsumer
-	cc.Subscribe(topic, consumer.MessageSelector{}, func(ctx context.Context, ext ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
-		for _, m := range ext {
-			handler(m)
-			fmt.Printf("receive msg topic %s tag %s broker %s queue %d body %s\n", m.Topic, m.GetTags(), m.Queue.BrokerName, m.Queue.QueueId, string(m.Body))
-		}
-		return consumer.ConsumeSuccess, nil
-	})
-}
+//type Cons struct {
+//	clientConsumer rocketmq.PushConsumer
+//}
+//
+//var Cs Cons
+//
+//func Initial(c *rmq.ConsumerConfig) (*Cons, error) {
+//	if c, err := rmq.NewConsumer(c); err == nil {
+//		Cs.clientConsumer = c
+//		return &Cs, nil
+//	} else {
+//		return nil, err
+//	}
+//}
+//
+//func (c *Cons) SetTopicConsumer(topic string, handler func(ext *primitive.MessageExt)) {
+//	cc := c.clientConsumer
+//	cc.Subscribe(topic, consumer.MessageSelector{}, func(ctx context.Context, ext ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+//		for _, m := range ext {
+//			handler(m)
+//			fmt.Printf("receive msg topic %s tag %s broker %s queue %d body %s\n", m.Topic, m.GetTags(), m.Queue.BrokerName, m.Queue.QueueId, string(m.Body))
+//		}
+//		return consumer.ConsumeSuccess, nil
+//	})
+//}
