df7ad86e05d9f4e4ef674f11041cb74166b39f62
hangaoshuai <hangaoshuai2022@163.com>
2022-11-02 16:57:26 +0800

    add mult gin

--- service/foo-multi/internal/handler/routes.go
+++ service/foo-multi/internal/handler/routes.go
@@ -15,6 +15,7 @@ func RegisterHandler(routers ginx.ZeroGinRouter) {
 		return ginx.Success(time.Now().Unix())
 	})
 
+	//访问路径 /config
 	routers.Handle(http.MethodGet, "/config", example.GetConfig)
 
 	//注意这时返回默认http code状态码200, 这里的code不是http code
@@ -22,7 +23,21 @@ func RegisterHandler(routers ginx.ZeroGinRouter) {
 
 	demogroup := routers.Group("/demogroup")
 	demogroup.Use(middleware.Auth, middleware.ContextM)
+
+	//访问路径 /demogroup/demoroute
 	demogroup.Handle(http.MethodGet, "/demoroute", example.DemoRoute)
 	//会返回http code 504错误码
 	demogroup.Handle(http.MethodGet, "/error", example.HttpCode)
+
+	//访问路径 /empty
+	emptyGroup := routers.Group("")
+	emptyGroup.Use(middleware.ContextM2)
+	emptyGroup.Handle(http.MethodGet, "/empty", func(c *gin.Context) ginx.Render {
+		//会拿到ContextM2中间件塞入gin.Contex的值
+		va, ok := c.Get("contextMiddle2")
+		if !ok {
+			return ginx.Error(ginx.NewGinError(500100, "内部错误"))
+		}
+		return ginx.Success(va)
+	})
 }
