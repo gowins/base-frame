a1535af40042b6ebeb64211666e8db9042ea296f
hangaoshuai <hangaoshuai2022@163.com>
2022-11-02 16:47:16 +0800

    change base-frame

--- service/bar-single/internal/handler/middleware/context_middle.go
+++ service/bar-single/internal/handler/middleware/context_middle.go
@@ -13,3 +13,10 @@ func ContextM(c *gin.Context) {
 	c.Set("contextMiddle", demoValue)
 	c.Next()
 }
+
+func ContextM2(c *gin.Context) {
+	demoValue := time.Now().String() + " contextMiddle2"
+	fmt.Printf("this auth context middleware\n")
+	c.Set("contextMiddle2", demoValue)
+	c.Next()
+}
